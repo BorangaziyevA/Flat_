c7346e716b7f24cd3ff1e0c6b623f6aa460bbd17
BorangaziyevA <45071543+BorangaziyevA@users.noreply.github.com>
2019-04-11 21:51:57 +0600

    Update flat_.h

--- Flat/flat_.h
+++ Flat/flat_.h
@@ -7,6 +7,13 @@ class flat_
 	room_* rooms;
 public:
 	flat_(int cost,int num);
+	flat_(const flat_ & obj);
 	~flat_();
+	void info();
+	int sumArea();
+	bool type(const flat_ &obj);
+	bool area(flat_ obj);
+	//bool cost(const flat_ &obj);
 };
+
 
