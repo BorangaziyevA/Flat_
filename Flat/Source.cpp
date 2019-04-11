b0bd658f748e17e10ce13a554983acfc5afe1c0a
BorangaziyevA <45071543+BorangaziyevA@users.noreply.github.com>
2019-04-11 21:52:59 +0600

    Update Source.cpp

--- Flat/Source.cpp
+++ Flat/Source.cpp
@@ -12,3 +12,26 @@ void main()
 	
 	system("pause");
 }
+
+#include "room_.h"
+#include "flat_.h"
+
+void main()
+{
+	//room_ a("Bathroom", 10);
+	//room_ b(a);
+	//b.info();
+	//cout << b.getArea() << endl;
+	//cout << b.getName() << endl;
+	//a.setArea(12);
+	//a.setName("Toilet");
+	//a.info();
+
+	flat_ a(5000, 2);
+	flat_ b(500,3);
+	a.info();
+	cout <<a.sumArea()<<endl;
+	cout << a.area(b)<<endl;
+	
+	system("pause");
+}
