a2905cdd78155c12fc578ee9c8fd014acd7a4ea2
BorangaziyevA <45071543+BorangaziyevA@users.noreply.github.com>
2019-06-07 15:52:54 +0600

    Update room_.cpp

--- Flat/room_.cpp
+++ Flat/room_.cpp
@@ -1,53 +1,77 @@
-#include "room_.h"
-
-
-
-room_::room_()
+#include "Room.h"
+#include <iostream>
+using namespace std;
+room::room()
 {
-	this->name = nullptr;
-	this->area = 0;
+	name = new char[strlen("hall") + 1];
+	strcpy_s(this->name, strlen("hall") + 1, "hall");
+	this->area = 20;
 }
 
-room_::room_(const char* name, int area)
+room::room(const char* name, int area)
 {
 	this->name = new char[strlen(name) + 1];
 	strcpy_s(this->name, strlen(name) + 1, name);
 	this->area = area;
 }
 
-room_::~room_()
+room::room(const room&obj)
 {
-
-	delete[] this->name;
-
+	this->name = new char[strlen(name) + 1];
+	strcpy_s(this->name, strlen(name + 1), obj.name);
+	this->area = obj.area;
 }
 
-void room_::info()
-{
-	cout << this->name << " " << this->area << endl;
-}
 
-char * room_::getName()
+void room::setName()
 {
-	return this->name;
-}
-
-int room_::getArea()
-{
-	return this->area;
-}
-
-void room_::setName(const char* name)
-{
-	if (this->name != nullptr)
+	char tmp[100];
+	if (name != nullptr)
 	{
-		delete[] this->name;
+		delete[] name;
 	}
-	this->name = new char[strlen(name) + 1];
-	strcpy_s(this->name, strlen(name) + 1, name);
+	cout << "Enter name of room" << endl;
+	cin >> tmp;
+	this->name = new char[strlen(name + 1)];
+	strcpy_s(this->name, strlen(tmp) + 1, tmp);
+}
+void room::setArea()
+{
+	int a = 0;
+	cout << "enter area of room" << endl;
+	cin >> a;
+	this->area = a;
 }
 
-void room_::setArea(int area)
+void room::rewrite(const room & p)
 {
-	this->area = area;
+	this->area = p.area;
+	if (strlen(this->name) != strlen(p.name)) {
+		delete[]this->name;
+		this->name = new char[strlen(p.name) + 1];
+	}
+	strcpy_s(this->name, strlen(p.name) + 1, p.name);
+}
+
+
+
+
+void room::info() const
+{
+	cout << "name:  " << name << "   area: " << area << endl;
+}
+
+int room::getArea()
+{
+	return area;
+}
+
+bool room::getName()
+{
+	return name;
+}
+
+room::~room() {
+
+	delete[]this->name;
 }
