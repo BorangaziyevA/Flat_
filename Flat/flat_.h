1833eed3e9a736a45bcc5e6faa98710f17167ac1
BorangaziyevA <45071543+BorangaziyevA@users.noreply.github.com>
2019-06-07 15:50:54 +0600

    Update flat_.h

--- Flat/flat_.h
+++ Flat/flat_.h
@@ -1,19 +1,64 @@
 #pragma once
-#include"room_.h"
-class flat_
-{
-	int cost;
-	int num;
-	room_* rooms;
+#include "Room.h"
+#include <iostream>
+using namespace std;
+
+class flat {
+	int price;
+	room *rooms;
+	int cntRooms;
 public:
-	flat_(int cost,int num);
-	flat_(const flat_ & obj);
-	~flat_();
-	void info();
-	int sumArea();
-	bool type(const flat_ &obj);
-	bool area(flat_ obj);
-	//bool cost(const flat_ &obj);
+	flat(int price) {
+		this->price = price;
+		this->rooms = nullptr;
+		this->cntRooms = 0;
+	}
+	void addRoom(const room &r) {
+		if (this->rooms == nullptr) {
+
+			this->rooms = new room[1];
+
+			rooms[0].rewrite(r);
+			cntRooms = 1;
+			return;
+		}
+		room *tmp = new room[cntRooms + 1];
+		for (size_t i = 0; i < cntRooms; i++)
+			tmp[i].rewrite(rooms[i]);
+
+		tmp[cntRooms].rewrite(r);
+		delete[]rooms;
+		rooms = tmp;
+		cntRooms++;
+	}
+	int getFlatArea() const {
+		int area = 0;
+		for (size_t i = 0; i < cntRooms; i++)
+		{
+			area += rooms[i].getArea();
+		}
+		return area;
+	}
+	int checkPrice(const flat& obj){
+		if (this->price==obj.price){
+			return true;}
+		else return false;}
+	
+	int checkArea(const flat& obj){
+		if (this->getFlatArea==obj.getFlatArea){return true;}
+		else return false;}
+	
+			
+
+			
+	void info() {
+		cout << "Flat amount = " << this->price
+			<< "\ncount of rooms = " << cntRooms
+			<< "flat area = " << this->getFlatArea()
+			<< "rooms:\n";
+		for (size_t i = 0; i < cntRooms; i++)
+		{
+			rooms[i].info();
+		}
+	}
 };
-
-
